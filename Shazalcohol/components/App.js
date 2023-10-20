8139a32ab96055cb425b6855137439b5cd86ed55
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-10-20 09:28:25 +0200

    Built structure for the project. Push of every file of the project.

---
+++ Shazalcohol/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CameraComponent from "./CameraComponent";
+
+export default function App() {
+  return (
+    <CameraComponent/>
+  );
+}
---
+++ Shazalcohol/components/CameraComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles/styles.css";
+
+export default function CameraComponent() {
+    return (
+        <p>
+            CameraComponent
+        </p>
+    );
+}
