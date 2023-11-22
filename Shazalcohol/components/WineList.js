f953071590d5559c5f6a35563d389a9b97854251
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-11-22 16:29:51 +0100

    Wine list dynamic + wine infos

--- Shazalcohol/components/WineList.js
+++ Shazalcohol/components/WineList.js
@@ -3,7 +3,52 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { Card } from 'react-native-elements';
 
 class WineList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wines: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchWines();
+    }
+
+    fetchWines = async () => {
+        try {
+            const response = await fetch('http://82.66.48.233:42690/getAllWines?offset=0&winesPerPage=10', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                const bodyData = JSON.parse(responseData[0].body);
+                this.setState({ wines: bodyData.wines });
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     render() {
+        const wineCards = this.state.wines && Array.isArray(this.state.wines) ? (
+            this.state.wines.map((wine, index) => (
+                <View key={index} style={{ width: '90%' }}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('WineScreen', { wine })}>
+                        <Card>
+                            <Card.Title>{wine.name + ' (' + wine.year + ')'}</Card.Title>
+                            <Card.Divider />
+                            <Text>{wine.type}</Text>
+                        </Card>
+                    </TouchableOpacity>
+                </View>
+            ))
+        ) : null;
+
         return (
             <View style={styles.container}>
                 {this.props.route.params.isAdmin && (
@@ -15,15 +60,7 @@ class WineList extends React.Component {
                     </View>
                 )}
                 <View style={{ marginBottom: 10 }} />
-                <View style={{ width: '90%' }}>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('WineScreen')}>
-                        <Card>
-                            <Card.Title>Wine 1 (2006)</Card.Title>
-                            <Card.Divider />
-                            <Text>Rouge</Text>
-                        </Card>
-                    </TouchableOpacity>
-                </View>
+                {wineCards}
             </View>
         );
     }
--- Shazalcohol/components/WineScreen.js
+++ Shazalcohol/components/WineScreen.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 class WineScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wines: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchWines();
+    }
+
+    fetchWines = async () => {
+        try {
+            const response = await fetch('http://82.66.48.233:42690/getWine?wineId=' + this.props.route.params.wine.id, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                const bodyData = JSON.parse(responseData[0].body);
+                this.setState({ wines: bodyData });
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     render() {
+        const wine = this.state.wines ? this.state.wines : null;
         return (
-            <View style={styles.container}>
-                <Text>C'est le vin rouge 1</Text>
-                <Text>Il est très bon</Text>
-                <Image source={require('../images/Wine1.png')} style={styles.image} />
-                <Text>Noté 4/5</Text>
+            <View>
+                {wine && (
+                    <React.Fragment>
+                        <Text>{this.props.route.params.wine.name}</Text>
+                        <Text>{this.props.route.params.wine.year}</Text>
+                        <Text>{wine.origin}</Text>
+                        <Text>{wine.price}€</Text>
+                    </React.Fragment>
+                )}
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    image: {
-        width: 400,
-        height: 400,
-    },
-});
 
 export default WineScreen;
