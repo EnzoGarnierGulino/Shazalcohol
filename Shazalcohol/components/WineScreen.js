3ad846b4c67d6a81cbbf29ce568e823cc8f9debb
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-11-24 11:22:15 +0100

    Added wine edition

--- Shazalcohol/components/WineScreen.js
+++ Shazalcohol/components/WineScreen.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
 
 class WineScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isAdmin: this.props.route.params.isAdmin,
-            wines: [],
-            name: '',
-            year: '',
-            origin: '',
-            price: '',
+            name: this.props.route.params.wine.name,
+            year: this.props.route.params.wine.year,
         };
     }
 
@@ -30,38 +27,84 @@ class WineScreen extends React.Component {
             if (response.ok) {
                 const responseData = await response.json();
                 const bodyData = JSON.parse(responseData[0].body);
-                this.setState({ wines: bodyData });
+                this.setState({
+                    origin: bodyData.origin,
+                    price: bodyData.price.toString(),
+                });
             }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
 
+    sendModifiedWine = async () => {
+        try {
+            const response = await fetch('http://82.66.48.233:42690/editWine', {
+                method: 'PUT',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: this.state.name,
+                    year: this.state.year,
+                    origin: this.state.origin,
+                    price: this.state.price,
+                    id: this.props.route.params.wine.id,
+                }),
+            });
+            if (response.ok) {
+                alert('Wine successfully modified!');
+                await this.props.navigation.navigate('WineList');
+            } else {
+                const responseData = await response.json();
+                const reason = responseData && responseData.reason ? responseData.reason : 'Unknown reason';
+                alert(`Wine modification failed (Error code: ${response.status})\nReason: ${reason}\nPlease try again.`);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     render() {
-        const wine = this.state.wines ? this.state.wines : null;
         return (
             <View>
-                {wine && (
-                    <React.Fragment>
-                        {this.state.isAdmin ? (
-                            <React.Fragment>
-                                <TextInput
-                                    style={styles.input}
-                                >{this.props.route.params.wine.name}</TextInput>
-                                <TextInput>{this.props.route.params.wine.year}</TextInput>
-                                <TextInput>{wine.origin}</TextInput>
-                                <TextInput>{wine.price}€</TextInput>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                <Text>{this.props.route.params.wine.name}</Text>
-                                <Text>{this.props.route.params.wine.year}</Text>
-                                <Text>{wine.origin}</Text>
-                                <Text>{wine.price}€</Text>
-                            </React.Fragment>
-                        )}
-                    </React.Fragment>
-                )}
+                <React.Fragment>
+                    {this.state.isAdmin ? (
+                        <React.Fragment>
+                            <TextInput
+                                style={styles.input}
+                                value={this.state.name}
+                                onChangeText={(name) => this.setState({name: name})}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                value={this.state.year.toString()}
+                                onChangeText={(year) => this.setState({year: year})}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                value={this.state.origin}
+                                onChangeText={(origin) => this.setState({origin: origin})}
+                            />
+                            <TextInput
+                                style={styles.input}
+                                value={this.state.price}
+                                onChangeText={(price) => this.setState({price: price})}
+                            />
+                            <Button
+                                title="Edit wine"
+                                onPress={() => this.sendModifiedWine()}/>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Text>{this.state.name}</Text>
+                            <Text>{this.state.year}</Text>
+                            <Text>{this.state.origin}</Text>
+                            <Text>{this.state.price}€</Text>
+                        </React.Fragment>
+                    )}
+                </React.Fragment>
             </View>
         );
     }
